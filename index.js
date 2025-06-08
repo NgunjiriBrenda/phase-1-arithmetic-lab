56ba1d8843f3c1d1f20ba2b9e34dae31c307c91c
brenda njoki <ngunjiribrenda5@gmail.com>
2025-06-08 14:33:05 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max (6, 10, 20, 16);
+console.log(max);
